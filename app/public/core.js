cf44612bce0f5f32947ece3014d415652bfd2a36
Cody Seavey <cody.seavey@pearson.com>
2017-06-23 15:54:06 -0500

    added waiting message

--- app/public/core.js
+++ app/public/core.js
@@ -5,15 +5,19 @@ function mainController($scope, $http) {
 
     $scope.initialize = function() {
         $scope.lyrics = {};
+        $scope.waiting = "";
     };
 
     $scope.getArtistLyrics = function() {
+        $scope.waiting = "Gathering results please wait.";
         $http.post('/api/count', $scope.formData)
             .success(function(data) {
                 $scope.formData = {};
                 $scope.lyrics = data;
+                $scope.waiting = ""
             })
             .error(function(data) {
+                $scope.waiting = ""
                 console.log('Error: ' + data);
             });
     };
